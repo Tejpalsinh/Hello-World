c3db70bdd965ab1be61932f57c5064f25d92ae3e
Tejpalsinh <40031464+Tejpalsinh@users.noreply.github.com>
2018-06-07 06:58:23 -0700

    Trying out..

---
+++ Main.cs
@@ -0,0 +1,7 @@
+public static void Main(String[] arg){
+
+int[] sortedArray = new int {3,5,6,7,9};
+int mid = ((sortedArray.lenght-1)-0)/2;
+System.out.println("Testing code" + sortedArray[mid]);//testing only...
+
+}
